9f0b3135351cde269121217620560598eda60754
James Womack <jwomack@netflix.com>
2015-03-19 15:45:47 -0700

    Restore componentRoutes-based rendering

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var server = require('./lib/reactiveRestifyServer');
 
 
 /*
- * Mirror the React Component paths in conf/viewRoutes.json
+ * Mirror the React Component paths in conf/componentRoutes.json
  * as Restify endpoints
  */
 server.loadViewRoutesAsRestEndpoints();
@@ -12,9 +12,9 @@ server.loadViewRoutesAsRestEndpoints();
  * Demonstrate one-shot component emission
  */
 server.get('/animal-farm', function(req,res,next){
-  var viewRouter = this.componentLayer.viewRouter;
-  var cat = viewRouter.componentFromPath('cat', {id: 'Gato'});
-  var dog = viewRouter.componentFromPath('dog', {id: 'Perro'});
+  var componentRouter = this.componentLayer.componentRouter;
+  var cat = componentRouter.componentFromPath('cat', {id: 'Gato'});
+  var dog = componentRouter.componentFromPath('dog', {id: 'Perro'});
   res.emit('components:ready', [cat,dog], {everybody:'Let\'s dance now'},  next);
 });
 
@@ -23,17 +23,15 @@ server.get('/animal-farm', function(req,res,next){
  * Demonstrate aggregation-based component rendering
  */
 server.param('aggregation', function(req,res,next){
-  var viewRouter = this.componentLayer.viewRouter;
-  var cat = viewRouter.componentFromPath('cat', {id: 'Gato'});
-  res.emit('component:ready', cat);
-  next();
+  var componentRouter = this.componentLayer.componentRouter;
+  var dog = componentRouter.componentFromPath('dog', {id: 'Perro'});
+  res.emit('component:ready', dog, next);
 });
 
 server.param('aggregation', function(req,res,next){
-  var viewRouter = this.componentLayer.viewRouter;
-  var dog = viewRouter.componentFromPath('dog', {id: 'Perro'});
-  res.emit('component:ready', dog);
-  next();
+  var componentRouter = this.componentLayer.componentRouter;
+  var cat = componentRouter.componentFromPath('cat', {id: 'Gato'});
+  res.emit('component:ready', cat, next);
 });
 
 server.get('/animal-farm/:aggregation', function(req,res,next){
